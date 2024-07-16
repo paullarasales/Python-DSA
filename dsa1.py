e042b00821e0fdb871c6dfbec14ad8092e949751
paullarasales <ahmadzufukli@gmail.com>
2024-07-16 12:00:00 +0800

    Backdated commit for July 16

--- dsa1.py
+++ dsa1.py
@@ -5,3 +5,10 @@ print("Basic List:", numbers)
 numbers.append(6)
 numbers.insert(0, 0)
 print("After Modifications:", numbers)
+
+
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return 1
+    return -1
