21c5020a6a37b59764ac390bab3a6a67b49f0281
paullarasales <rkise2131@gmail.com>
2025-01-09 22:43:10 +0800

    day 2 dsa

--- dsa2.py
+++ dsa2.py
@@ -27,3 +27,64 @@ class Human:
 bob = Human(2, 2)
 print(bob.legs)
 print(bob.arms)
+
+class Plane: 
+    def __init__(self):
+        self.wings = 2
+
+        self.drive()
+        self.flaps()
+        self.wheels()
+
+    def drive(self):
+        print('Accelarating')
+    
+    def flaps(self):
+        print('Changing flaps')
+
+    def wheels(self):
+        print('Closing wheels')
+
+ba = Plane()
+
+class Bug:
+    def __init__(self):
+        self.wings = 4
+
+class Human:
+    def __init__(self):
+        self.legs = 2
+        self.arms = 2
+    
+bob = Human()
+tom = Bug()
+
+print(tom.wings)
+print(bob.arms)
+
+f = 101
+print(f)
+def local():
+    global f
+    print(f)
+    f = "changing global variable"
+
+local()
+print(f)
+
+class Student:
+    name = "Student"
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    def avg(self):
+        return (self.a + self.b)/2
+    
+    @classmethod
+    def info(cls):
+        return cls.name
+    
+sl = Student(10, 20)
+print(sl.avg())
+print(Student.info())
