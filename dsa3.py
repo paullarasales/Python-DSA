e3fa459ea5bdf36180f84d92807847c0389ce8b3
paullarasales <rkise2131@gmail.com>
2025-01-10 13:01:17 +0800

    done with the exercise 1

--- dsa3.py
+++ dsa3.py
@@ -16,3 +16,23 @@ buddy = Dog("Buddy", 9)
 
 print(miles.description())
 print(miles.speak("Arf Arf"))
+
+names = ["Miles", "Buddy", "Jack"]
+print(names)
+print(miles)
+
+# Exercice 1
+# Create a Car Class
+class Car:
+    def __init__(self, color, mileage):
+        self.color = color
+        self.mileage = mileage
+
+    def describe(self):
+        return f"The {self.color} has {self.mileage} miles"
+
+car_1 = Car("Blue", 20000)
+car_2 = Car("Red", 30000)
+
+print(car_1.describe())
+print(car_2.describe())
